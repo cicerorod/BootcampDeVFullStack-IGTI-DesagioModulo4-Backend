b24a88f93ce4d09146aa616e15579fd3e2dcab85
cicerorod <cicerorod@gmail.com>
2020-07-04 22:13:06 -0300

    primeiro commit

---
+++ models/gradeModels.js
@@ -0,0 +1,32 @@
+export default (mongoose) => {
+  const gradeSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("o valor deve ser superior a 0");
+        }
+      },
+    },
+    lastModified: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+  const Grade = mongoose.model("grade", gradeSchema, "grade");
+  return Grade;
+};
